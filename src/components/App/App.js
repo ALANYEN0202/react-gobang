a51b788b3cecd446f40103d837f3def704f365a6
顏鈺倫 <yanyulun@yanyulundeMacBook-Pro.local>
2020-11-17 00:23:32 +0800

    cssAndState

---
+++ src/components/App/App.js
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+import { MEDIA_QUERY_MD } from '../constants/style'
+
+const Title = styled.h1`
+  width: 100%;
+  text-align: center;
+`
+const Wrapper = styled.div`
+  width: 640px;
+  margin: 0 auto;
+  text-align: center;
+  ${MEDIA_QUERY_MD} {
+   width: 440px;
+   padding: 0px;
+   margin: 0 auto;
+  }
+`
+const Button = styled.button`
+  width: 34px;
+  height: 34px;
+  background-color: #FF9224;
+  margin-left: -1px;
+  margin-top: -1px;
+  padding: 4px;
+  line-height: 35px;
+
+  ${MEDIA_QUERY_MD} {
+    width: 24px;
+    height: 24px;
+    line-height: 15px;
+  }
+
+  &:hover {
+    background-color: #4F4F4F;
+  }
+`;
+
+const NextPlayer = styled.div`
+  text-align: center;
+`
+
+function ReaderSquare({ onClick, value, children }) {
+  return (
+    <Button 
+    value={value}
+    onClick={onClick}
+    >
+    {children}
+    </Button>
+  )
+}
+
+
+function App() {
+  const [board, setBoard] = useState(Array(19).fill(Array(19).fill(null)))
+  const [whiteIsNext, setWhiteIsNext] = useState(true)
+
+  function updateBoard(x, y, newValue) {
+  const newBoard = JSON.parse(JSON.stringify(board))
+  newBoard[y][x] = newValue
+  setBoard(newBoard)
+}
+
+function Board() {
+  const handleButtonClick = (arr) => {
+    updateBoard(arr[0], arr[1], whiteIsNext ? "O": "X")
+    setWhiteIsNext(!whiteIsNext)
+  }
+  let arr = []
+  for(let i = 0; i < 19; i++) {
+    arr[i] = []
+    for(let j= 0; j < 19; j++) {
+    arr[i][j] = <ReaderSquare 
+                  key={`${i},${j}`} 
+                  value={[i, j]} 
+                  onClick={() => {
+                    handleButtonClick([i, j])
+                  }}
+                  children = {board[j][i]}
+                />
+    }
+  }
+  return arr
+}
+  return (
+  <div className="App">
+    <Title>五子棋遊戲</Title>
+  <NextPlayer>NextPlayer is {whiteIsNext? "白子" : "黑子"}</NextPlayer>
+    <Wrapper>
+      <Board />
+    </Wrapper>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/constants/style.js
@@ -0,0 +1,3 @@
+export const MEDIA_QUERY_MD = "@media screen and (max-width: 760px)";
+export const MEDIA_QUERY_LG = "@media screen and (max-width: 1000px)";
+
